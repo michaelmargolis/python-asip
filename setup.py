bf082e39551b0aff60f3665e3266e289065519cd
Adam Jarzebak <adam@jarzebak.eu>
2018-07-25 01:55:44 +0100

    New version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="python_asip_client",
-    version="1.0.0",
+    version="1.0.1",
     author="Gianluca Barbon, Adam Jarzebak, Franco Raimondi",
     author_email="adam@jarzebak.eu",
     description="Serial manager for Mirto robot services",
